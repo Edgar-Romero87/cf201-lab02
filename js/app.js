354e4bdc4f88c7499ad5f7ecd7eb2a89d5ab13b9
Samsontek <spencer.d.rhodes@gmail.com>
2020-04-29 14:13:39 -0700

    Added an unordered list and an ordered list. Tweaked CSS to format the lists. TO DO: extend app.js for lab-03 requirements.

--- js/app.js
+++ js/app.js
@@ -57,3 +57,23 @@ checkAnswer(firstProjects, false);
 
 // Thank the user for playing the game.
 alert("Thanks for playing with me " + username + "! You can start the game again any time you like by clicking the link at the top of the page.");
+
+
+
+
+
+
+
+// As a user, I would like to be guided to an answer through a series of feedback responses so that I can learn more about the site owner.
+// Add a 6th question to the guessing game that takes in a numeric input by prompting the user to guess a number.
+// Indicates through an alert if the guess is “too high” or “too low”.
+// It should give the user exactly four opportunities to get the correct answer.
+// After all attempts have been exhausted, tell the user the correct answer. Consider using a loop of some sort.
+// As a user, I would like to guess the answer to a question that could have many possibilities so that I can have fun with with a guessing game.
+// Add a 7th question that has multiple possible correct answers that are stored in an array.
+// Give the user 6 attempts to guess the correct answer.
+// The guesses will end once the user guesses a correct answer or they run out of attempts.
+// Display all the possible correct answers to the user.
+// Consider using a loop of some sort for this question.
+// As a user, I would like to know my final score so that I can know how well I did.
+// Keep track of the total number of correct answers. At the end tell them how many they got correct out of the 7 questions asked.

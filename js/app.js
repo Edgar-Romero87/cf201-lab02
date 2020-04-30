ebf973a4e5b6fa1fb77977f610e477891bb5bfbb
Samsontek <spencer.d.rhodes@gmail.com>
2020-04-30 13:22:50 -0700

    Found a line of code in app.js that I'd written as a demonstration to another student. Deleted that. Nothing in this commit, but pushing just so git will stop tracking app.js as altered before we start pair programming.

--- js/app.js
+++ js/app.js
@@ -38,6 +38,7 @@ function checkAnswer(answer, correctAnswer, questionNumber){
 
 
 // Code goes here:
+
 // Greet the user and get their name.
 username = prompt("Welcome! What is your name?");
 if (confirm("Thanks for visiting my page, " + username + "! I'd like to invite you to play a game with me. I'm going to ask you five questions about the information on my page. You can retake the quiz as many times as you like.")){
@@ -68,7 +69,7 @@ if (confirm("Thanks for visiting my page, " + username + "! I'd like to invite y
   checkAnswer(firstProjects, false, 4);
 
   
- //////////////////////
+ 
   // Lucky number game.
   console.log(luckyNumber);
   for (var i=0; i<4; i++){
@@ -100,10 +101,6 @@ if (confirm("Thanks for visiting my page, " + username + "! I'd like to invite y
     
     
   }
-////////////////////////////////////
-
-
-
 
   // A guessing game with multiple correct answers.
   var famousDevelopers = [['Dennis Ritchie','Dennis MacAlistair Ritchie was an American computer scientist who is credited for shaping and pioneering the digital era. He created the most commonly used C programming language that is used today in various software applications, embedded system development, operating systems, and has influenced most modern programming languages.'],
@@ -165,17 +162,3 @@ if (confirm("Thanks for visiting my page, " + username + "! I'd like to invite y
 
   alert("Thanks for playing with me, " + username + "! You scored " + playerScore + " correct out of 7 questions. You can retake the quiz any time you like by clicking the link at the top of the page.")
 }
-
-
-
-// As a user, I would like to guess the answer to a question that could have many possibilities so that I can have fun with with a guessing game.
-
-
-// Add a 7th question that has multiple possible correct answers that are stored in an array.
-// Give the user 6 attempts to guess the correct answer.
-// The guesses will end once the user guesses a correct answer or they run out of attempts.
-// Display all the possible correct answers to the user.
-// Consider using a loop of some sort for this question.
-// As a user, I would like to know my final score so that I can know how well I did.
-// Keep track of the total number of correct answers. At the end tell them how many they got correct out of the 7 questions asked.
-
